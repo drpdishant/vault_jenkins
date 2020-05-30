da525840fac1ec53be75f218a1050e106dbd83e4
Dishant Pandya <dishant@localhost.localdomain>
2020-05-31 00:18:15 +0530

    update

--- vault_read.sh
+++ vault_read.sh
@@ -12,7 +12,8 @@ f) KV_FIELD=${OPTARG};;
 esac
 done
 
-#curl --request POST --data @payload.json https://addwebprojects.com:8200/v1/auth/approle/login | jq 
-# ACCESS_TOKEN="$(vault write -format=json auth/approle/login role_id=${ROLE_ID} secret_id=${SECRET_ID} | jq -r .auth.client_token)"
-ACCESS_TOKEN="$(curl -sL --request POST --data @payload.json ${VAULT_URL}/v1/auth/approle/login | jq -r .auth.client_token)"
+curl -Lo vault.zip https://releases.hashicorp.com/vault/1.4.2/vault_1.4.2_linux_amd64.zip
+unzip vault.zip
+ACCESS_TOKEN="$(./vault write -format=json auth/approle/login role_id=${ROLE_ID} secret_id=${SECRET_ID} | jq -r .auth.client_token)"
+# ACCESS_TOKEN="$(curl -sL --request POST --data @payload.json ${VAULT_URL}/v1/auth/approle/login | jq -r .auth.client_token)"
 curl -sL --header "X-Vault-Token: ${ACCESS_TOKEN}" ${VAULT_URL}/v1/${KV_PATH}/data/${KV_NAME}?version=1 | jq -r .data.data.${KV_FIELD}
