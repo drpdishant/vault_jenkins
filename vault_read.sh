538e53f57429c33276150228c2fdfe539225be26
Dishant Pandya <dishant@localhost.localdomain>
2020-05-30 20:14:25 +0530

    update vault_read

--- vault_read.sh
+++ vault_read.sh
@@ -11,5 +11,14 @@ n) KV_NAME=${OPTARG};;
 f) KV_FIELD=${OPTARG};;
 esac
 done
-ACCESS_TOKEN="$(vault write -format=json auth/approle/login role_id=${ROLE_ID} secret_id=${SECRET_ID} | jq -r .auth.client_token)"
-curl -sL --header "X-Vault-Token: ${ACCESS_TOKEN}" https://addwebprojects.com:8200/v1/${KV_PATH}/data/${KV_NAME}?version=1 | jq -r .data.data.${KV_FIELD}
+tee payload.json <<"EOF"
+{
+  "role_id": "${ROLE_ID}",
+  "secret_id": "${SECRET_ID}"
+}
+EOF
+curl --request POST --data @payload.json ${VAULT_URL}/v1/auth/approle/login | jq 
+#curl --request POST --data @payload.json https://addwebprojects.com:8200/v1/auth/approle/login | jq 
+# ACCESS_TOKEN="$(vault write -format=json auth/approle/login role_id=${ROLE_ID} secret_id=${SECRET_ID} | jq -r .auth.client_token)"
+ACCESS_TOKEN="$(curl -sL --request POST --data @payload.json ${VAULT_URL}/v1/auth/approle/login | jq -r .auth.client_token)"
+curl -sL --header "X-Vault-Token: ${ACCESS_TOKEN}" ${VAULT_URL}/v1/${KV_PATH}/data/${KV_NAME}?version=1 | jq -r .data.data.${KV_FIELD}
