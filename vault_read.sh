d18e75ce8ecb8d440682a5b2e87f9631feec85f6
Dishant Pandya <dishant@localhost.localdomain>
2020-05-31 00:26:32 +0530

    update

--- vault_read.sh
+++ vault_read.sh
@@ -14,7 +14,7 @@ done
 
 curl -Lo vault.zip https://releases.hashicorp.com/vault/1.4.2/vault_1.4.2_linux_amd64.zip
 unzip -o vault.zip
-VAULT_ADDR=$VAULT_URL
+export VAULT_ADDR=$VAULT_URL
 ACCESS_TOKEN="$(./vault write -format=json auth/approle/login role_id=${ROLE_ID} secret_id=${SECRET_ID} | jq -r .auth.client_token)"
 # ACCESS_TOKEN="$(curl -sL --request POST --data @payload.json ${VAULT_URL}/v1/auth/approle/login | jq -r .auth.client_token)"
 curl -sL --header "X-Vault-Token: ${ACCESS_TOKEN}" ${VAULT_URL}/v1/${KV_PATH}/data/${KV_NAME}?version=1 | jq -r .data.data.${KV_FIELD}
