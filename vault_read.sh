31e311d52853cd2f62187b18a8b5051bb71ce344
Dishant Pandya <dishant@localhost.localdomain>
2020-05-30 23:42:41 +0530

    update vault read

--- vault_read.sh
+++ vault_read.sh
@@ -13,10 +13,13 @@ esac
 done
 tee payload.json <<"EOF"
 {
-  "role_id": "${ROLE_ID}",
-  "secret_id": "${SECRET_ID}"
+  "role_id": "_ROLE_ID_",
+  "secret_id": "_SECRET_ID_"
 }
 EOF
+sed -i 's%_ROLE_ID_%${ROLE_ID}%g'  payload.json
+sed -i 's%_SECRET_ID_%${SECRET_ID}%g'  payload.json
+cat payload.json
 #curl --request POST --data @payload.json https://addwebprojects.com:8200/v1/auth/approle/login | jq 
 # ACCESS_TOKEN="$(vault write -format=json auth/approle/login role_id=${ROLE_ID} secret_id=${SECRET_ID} | jq -r .auth.client_token)"
 ACCESS_TOKEN="$(curl -sL --request POST --data @payload.json ${VAULT_URL}/v1/auth/approle/login | jq -r .auth.client_token)"
