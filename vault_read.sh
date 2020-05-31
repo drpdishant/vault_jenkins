dbd2c84d806107f78bc42636b605ffb9a1efbda9
Dishant Pandya <dishant@localhost.localdomain>
2020-05-31 10:55:49 +0530

    update

--- vault_read.sh
+++ vault_read.sh
@@ -3,7 +3,7 @@ while getopts u:r:s:p:n:f: option
 do
 case "${option}"
 in
-u) VAULT_ADDR=${OPTARG};;
+u) VAULT_SERVER=${OPTARG};;
 r) ROLE_ID=${OPTARG};;
 s) SECRET_ID=${OPTARG};;
 p) KV_PATH=${OPTARG};;
@@ -11,7 +11,8 @@ n) KV_NAME=${OPTARG};;
 f) KV_FIELD=${OPTARG};;
 esac
 done
-export VAULT_ADDR=$VAULT_ADDR
+
+export VAULT_ADDR=$VAULT_SERVR
 ACCESS_TOKEN="$(vault write -format=json auth/approle/login role_id=${ROLE_ID} secret_id=${SECRET_ID} | jq -r .auth.client_token)"
 # ACCESS_TOKEN="$(curl -sL --request POST --data @payload.json ${VAULT_URL}/v1/auth/approle/login | jq -r .auth.client_token)"
 curl -sL --header "X-Vault-Token: ${ACCESS_TOKEN}" ${VAULT_URL}/v1/${KV_PATH}/data/${KV_NAME}?version=1 | jq -r .data.data.${KV_FIELD}
