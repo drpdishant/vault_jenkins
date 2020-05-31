38a150eb7edc9ccef39d8ee6094b4fa637ccd181
Dishant Pandya <dishant@localhost.localdomain>
2020-05-31 11:03:03 +0530

    update

--- vault_read.sh
+++ vault_read.sh
@@ -15,4 +15,4 @@ done
 export VAULT_ADDR=$VAULT_SERVER
 ACCESS_TOKEN="$(vault write -format=json auth/approle/login role_id=${ROLE_ID} secret_id=${SECRET_ID} | jq -r .auth.client_token)"
 # ACCESS_TOKEN="$(curl -sL --request POST --data @payload.json ${VAULT_URL}/v1/auth/approle/login | jq -r .auth.client_token)"
-curl -sL --header "X-Vault-Token: ${ACCESS_TOKEN}" ${VAULT_URL}/v1/${KV_PATH}/data/${KV_NAME}?version=1 | jq -r .data.data.${KV_FIELD}
+curl -sL --header "X-Vault-Token: ${ACCESS_TOKEN}" ${VAULT_SERVER}/v1/${KV_PATH}/data/${KV_NAME}?version=1 | jq -r .data.data.${KV_FIELD}
