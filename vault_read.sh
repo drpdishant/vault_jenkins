7ee7d778dd32f4f19efd6ca87094b7e1b52d28ee
Dishant Pandya <dishant@localhost.localdomain>
2020-05-31 10:38:20 +0530

    update vault_read.sh

--- vault_read.sh
+++ vault_read.sh
@@ -11,9 +11,6 @@ n) KV_NAME=${OPTARG};;
 f) KV_FIELD=${OPTARG};;
 esac
 done
-
-curl -Lo vault.zip https://releases.hashicorp.com/vault/1.4.2/vault_1.4.2_linux_amd64.zip
-unzip -o vault.zip
 export VAULT_ADDR=$VAULT_URL
 ACCESS_TOKEN="$(./vault write -format=json auth/approle/login role_id=${ROLE_ID} secret_id=${SECRET_ID} | jq -r .auth.client_token)"
 # ACCESS_TOKEN="$(curl -sL --request POST --data @payload.json ${VAULT_URL}/v1/auth/approle/login | jq -r .auth.client_token)"
